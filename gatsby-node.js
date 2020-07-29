f0baa07a271e24c83306c9f7697593fa30af8800
08561225726 <giovani.racca@serpro.gov.br>
2020-07-29 18:48:56 -0300

    adiciona filtro de categoria na query

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,6 +12,7 @@ exports.createPages = async ({ graphql, actions }) => {
     `
       {
         allMarkdownRemark(
+          filter: { frontmatter: {category: {eq: "post"}} },
           sort: { fields: [frontmatter___date], order: DESC }
           limit: 1000
         ) {
@@ -30,6 +31,29 @@ exports.createPages = async ({ graphql, actions }) => {
         tagsGroup: allMarkdownRemark(limit: 2000) {
           group(field: frontmatter___tags) {
             fieldValue
+          }
+        }
+      }
+    `
+  )
+
+  const readingsResult = await graphql(
+    `
+      {
+        allMarkdownRemark(
+          filter: { frontmatter: {category: {eq: "readings"}} },
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+              }
+            }
           }
         }
       }
@@ -58,6 +82,24 @@ exports.createPages = async ({ graphql, actions }) => {
         next,
       },
     })    
+  })
+
+  // Create blog posts pages.
+  const readings = readingsResult.data.allMarkdownRemark.edges
+
+  // Make tag pages
+  readings.forEach((post, index) => {
+    const previous = index === readings.length - 1 ? null : readings[index + 1].node
+    const next = index === 0 ? null : readings[index - 1].node
+    createPage({
+      path: post.node.fields.slug,
+      component: blogPost,
+      context: {
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
+    })
   })
 
   // Extract tag data from query
