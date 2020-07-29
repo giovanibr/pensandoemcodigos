523c51a964ad17813e3fd3c7f57842693a6012de
08561225726 <giovani.racca@serpro.gov.br>
2020-07-29 18:52:05 -0300

    cria pagina readings

---
+++ src/pages/readings.tsx
@@ -0,0 +1,126 @@
+// Gatsby supports TypeScript natively!
+import React from "react"
+import { PageProps, Link, graphql } from "gatsby"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+
+type Data = {
+  site: {
+    siteMetadata: {
+      title: string
+      menuLinks: {
+        name: string
+        link: string
+      }
+    }
+  }
+  allMarkdownRemark: {
+    edges: {
+      node: {
+        excerpt: string
+        frontmatter: {
+          title: string
+          date: string
+          description: string
+          category: string
+        }
+        fields: {
+          slug: string
+        }
+      }
+    }[]
+  }
+}
+
+const BlogIndex = ({ data, location }: PageProps<Data>) => {
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMarkdownRemark.edges
+  const menuLinks = data.site.siteMetadata.menuLinks
+
+  return (
+    <Layout location={location} title={siteTitle} menu={menuLinks}>
+      <SEO title="All posts" />
+
+      <div
+        style={{
+          marginLeft: rhythm(.5),
+          maxWidth: rhythm(20)
+        }}
+      >
+
+        <h2>Leituras</h2>
+
+        <p>
+          Semanário com os artigos e <i>posts</i> mais interessantes que eu me deparei pela internet.
+        </p>
+
+      </div>
+
+      
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return (
+          <article key={node.fields.slug} style={{marginLeft: rhythm(.5)}}>
+            <header>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+            </header>
+            <section>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </section>
+          </article>
+        )
+      })}
+    </Layout>
+  )
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      filter: {
+        frontmatter: {category: {eq: "readings"}}
+      },
+      sort: { 
+        fields: [frontmatter___date], order: DESC 
+      }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "DD/MM/YY")
+            title
+            description
+            tags
+            category
+          }
+        }
+      }
+    }
+  }
+`
